212662d1d74e7dc2ef9d761fbff3afabc7f47180
pifra004 <pierre-yanick.franke004@stud.fh-dortmund.de>
2023-06-11 22:00:47 +0200

    added classes

--- Schach/src/Figur/König.java
+++ Schach/src/Figur/König.java
@@ -2,6 +2,8 @@ package Figur;
 
 public class König extends Figur{
 	
+	private boolean atStart=true;
+	
 	public König(String team, int xPos, int yPos) {
 		super(team, xPos, yPos);
 	}
--- Schach/src/Figur/Turm.java
+++ Schach/src/Figur/Turm.java
@@ -2,6 +2,8 @@ package Figur;
 
 public class Turm extends Figur{
 	
+	private boolean atStart=true;
+	
 	public Turm(String team, int xPos, int yPos) {
 		super(team, xPos, yPos);
 	}
